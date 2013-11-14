d35c4d18717a2d06c3a388c65417a7ea068ad4cb
Max Hoskins <maxim.hoskins@gmail.com>
2013-11-14 21:59:26 +0100

    first commit

---
+++ VrpRepSolutionChecker/src/vrpRep/solutionChecker/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package vrpRep.solutionChecker;
+
+/**
+ * @author Maxim
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
