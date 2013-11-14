c66b744cd9cfa9dfd0733415ed247e8e89f803e2
Max Hoskins <maxim.hoskins@gmail.com>
2013-11-14 22:04:29 +0100

    Updated comment params

--- VrpRepSolutionChecker/src/vrpRep/solutionChecker/Test.java
+++ VrpRepSolutionChecker/src/vrpRep/solutionChecker/Test.java
@@ -1,17 +1,11 @@
-/**
- * 
- */
 package vrpRep.solutionChecker;
 
 /**
- * @author Maxim
+ * @author Maxim HOSKINS, Romain LIENARD, Raphael MOLY and Alexandre RENAUD
  *
  */
 public class Test {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
