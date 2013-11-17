aeb803801db6a24c9770546238358d27e96894fe
maxhoskins <maxim.hoskins@gmail.com>
2013-11-17 18:39:01 +0100

    Created instance reader and DefaultInstance Added JUnit test classes. Created JUnit test to test instance reader.

--- VrpRepSolutionChecker/src/vrpRep/solutionChecker/instance/DefaultInstance.java
+++ VrpRepSolutionChecker/src/vrpRep/solutionChecker/instance/DefaultInstance.java
@@ -3,10 +3,83 @@
  */
 package vrpRep.solutionChecker.instance;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.bind.JAXBException;
+
+import vrpRep.schema.Instance;
+import vrpRep.schema.Instance.Fleet.Vehicle;
+import vrpRep.schema.Instance.Network.Links.Link;
+import vrpRep.schema.Instance.Network.Nodes.Node;
+import vrpRep.schema.Instance.Requests.Request;
+import vrpRep.solutionChecker.instanceReader.InstanceReader;
+
 /**
+ * Class used to store XML instance data.
  * @author Maxim HOSKINS, Romain LIENARD, Raphael MOLY and Alexandre RENAUD
  *
  */
-public abstract class DefaultInstance {
+public class DefaultInstance {
 
+	
+	/**
+	 * Master node containing all xml file info
+	 */
+	protected Instance instance;
+	
+
+	
+	
+	/**
+	 * Constructor that creates xml tree on class instantiation 
+	 * @param xmlFile xml file to extract
+	 */
+	public DefaultInstance(File xmlFile){	
+		try {
+			InstanceReader<Instance> iR = new InstanceReader<Instance>();
+			this.instance = iR.unmarshallDocument(xmlFile, Instance.class.getName());
+		} catch (ClassNotFoundException | JAXBException | IOException e) {
+			e.printStackTrace();
+		}		
+	}
+	
+	
+	
+	
+	/**
+	 * 
+	 * @return list containing all nodes
+	 */
+	public List<Node> getNodes(){
+		return instance.getNetwork().getNodes().getNode();
+	}
+	
+	
+	/**
+	 * 
+	 * @return list containing all links
+	 */
+	public List<Link> getLinks(){
+		return instance.getNetwork().getLinks().getLink();
+	}
+	
+	
+	/**
+	 * 
+	 * @return list containing all vehicles
+	 */
+	public List<Vehicle> getFleet(){
+		return instance.getFleet().getVehicle();
+	}
+	
+	
+	/**
+	 * 
+	 * @return list containing all requests
+	 */
+	public List<Request> getResquests(){
+		return instance.getRequests().getRequest();
+	}
 }
--- VrpRepSolutionChecker/src/vrpRep/solutionChecker/instanceReader/StandardInstanceReader.java
+++ VrpRepSolutionChecker/src/vrpRep/solutionChecker/instanceReader/StandardInstanceReader.java
@@ -3,10 +3,37 @@
  */
 package vrpRep.solutionChecker.instanceReader;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.zip.ZipInputStream;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import vrpRep.schema.Instance;
+import vrpRep.schema.Instance.Fleet.Vehicle;
+import vrpRep.schema.Instance.Network.Links.Link;
+import vrpRep.schema.Instance.Network.Nodes;
+import vrpRep.schema.Instance.Network.Nodes.Node;
+import vrpRep.schema.Instance.Requests.Request;
+
 /**
  * @author Maxim HOSKINS, Romain LIENARD, Raphael MOLY and Alexandre RENAUD
+ * @param <T>
  *
  */
 public class StandardInstanceReader implements IInstanceReader {
+
+	@Override
+	public void unmarshallDocument(File xmlFile) throws JAXBException,
+			IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
