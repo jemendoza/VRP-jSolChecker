c3a6369d4ebb9d69018325ad6403aa864a203710
Alexandre <Alexandre@172.18.9.63>
2013-11-26 15:59:41 +0100

    Departure/Arrival constraint

--- VrpRepSolutionChecker/src/vrpRep/solutionChecker/constraint/DepartureArrivalNode.java
+++ VrpRepSolutionChecker/src/vrpRep/solutionChecker/constraint/DepartureArrivalNode.java
@@ -4,6 +4,12 @@
 package vrpRep.solutionChecker.constraint;
 
 
+import java.math.BigInteger;
+
+import vrpRep.schema.instance.Instance;
+import vrpRep.schema.instance.Instance.Fleet.Vehicle;
+import vrpRep.schema.solution.Solution;
+import vrpRep.schema.solution.Solution.Routes.Route;
 import vrpRep.solutionChecker.instance.StandardInstance;
 import vrpRep.solutionChecker.solution.DefaultSolution;
 
@@ -16,10 +22,31 @@ public class DepartureArrivalNode implements IConstraint {
 	/* (non-Javadoc)
 	 * @see vrpRep.solutionChecker.constraint.IConstraint#evaluate(vrpRep.solutionChecker.instance.DefaultInstance, vrpRep.solutionChecker.solution.DefaultSolution)
 	 */
+	private Instance instance;
+	private Solution solution;
+
 	@Override
 	public void evaluate(StandardInstance inst, DefaultSolution sol) {
-		// TODO Auto-generated method stub
-		
+		instance = (Instance) inst.getInstance();
+		solution = sol.getSolution();
+		boolean b = test();
+
 	}
 
+	private boolean test() {
+		for(Route r: solution.getRoutes().getRoute()){
+			BigInteger b = r.getType();
+			int nodeStart = Integer.parseInt(r.getNode().get(0).getContent());
+			int nodeArrival = Integer.parseInt(r.getNode().get(r.getNode().size()-1).getContent());
+			for(Vehicle v : instance.getFleet().getVehicle()){
+				if(v.getType().equals(b)){
+					if(!v.getArrivalNode().equals(nodeArrival) || !v.getDepartureNode().equals(nodeStart))
+						return false;
+
+				}
+			}
+			
+		}
+		return true;
+	}
 }
