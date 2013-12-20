d9c2af23fe31cae02f194623b3b44030d59a419d
maxhoskins <maxim.hoskins@gmail.com>
2013-12-20 12:30:16 +0100

    added solution classes

---
+++ VrpRepSolutionChecker/src/vrpRep/solution/v2/Solution.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package vrpRep.solution.v2;
+
+import java.util.ArrayList;
+
+/**
+ * Represents the default solution
+ * 
+ * @author Maxim HOSKINS, Romain LIENARD, Raphael MOLY and Alexandre RENAUD
+ * 
+ */
+public class Solution {
+
+	/**
+	 * Objective function value
+	 */
+	private double				of;
+	/**
+	 * List of routes
+	 */
+	private ArrayList<Route>	routes;
+
+	/**
+	 * 
+	 */
+	public Solution() {
+		this.routes = new ArrayList<Route>();
+	}
+
+	/**
+	 * @return the of
+	 */
+	public double getOf() {
+		return of;
+	}
+
+	/**
+	 * @param of
+	 *            the of to set
+	 */
+	public void setOf(double of) {
+		this.of = of;
+	}
+
+	/**
+	 * @return the routes
+	 */
+	public ArrayList<Route> getRoutes() {
+		return routes;
+	}
+
+	/**
+	 * Adds route to list of routes
+	 * 
+	 * @param r
+	 *            new route
+	 */
+	public void addRoute(Route r) {
+		this.routes.add(r);
+	}
+}
