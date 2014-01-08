9f53534d397b4b559255000d3889c823a4e61aa2
Raphael <Raphael@172.18.114.171>
2014-01-08 16:18:31 +0100

    Test

--- VrpRepSolutionChecker/src/sandbox/TestMain.java
+++ VrpRepSolutionChecker/src/sandbox/TestMain.java
@@ -6,11 +6,10 @@ package sandbox;
 import java.io.File;
 
 import vrpRep.fileReaders.InstanceReader;
-import vrpRep.schema.instance.Instance;
 
 /**
  * @author Maxim HOSKINS, Romain LIENARD, Raphael MOLY and Alexandre RENAUD
- *
+ * 
  */
 public class TestMain {
 
@@ -18,11 +17,13 @@ public class TestMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		InstanceReader inst = new InstanceReader(new File("./schemaFiles/A-n32-k5.xml.zip"));
-		
-		Instance.Network.Descriptor des = ((Instance)inst.getInstance()).getNetwork().getDescriptor();
-		System.out.println(des.getCustom().getAny().get(0).getTextContent());
-		
+		InstanceReader inst = new InstanceReader(new File(
+				"./schemaFiles/A-n32-k5.xml.zip"));
+
+		// Instance.Network.Descriptor des =
+		// ((Instance)inst.getInstance()).getNetwork().getDescriptor();
+		// System.out.println(des.getCustom().getAny().get(0).getTextContent());
+
 	}
 
 }
--- VrpRepSolutionChecker/src/vrpRep/solutionChecker/constraint/Capacity.java
+++ VrpRepSolutionChecker/src/vrpRep/solutionChecker/constraint/Capacity.java
@@ -3,24 +3,26 @@
  */
 package vrpRep.solutionChecker.constraint;
 
-
-import vrpRep.fileReaders.InstanceReader;
-import vrpRep.solutionChecker.solution.DefaultSolution;
-
 /**
  * Class used to evaluate capacity constraints
+ * 
  * @author Maxim HOSKINS, Romain LIENARD, Raphael MOLY and Alexandre RENAUD
- *
+ * 
  */
-public class Capacity implements IConstraint {
+public abstract class Capacity implements IConstraint {
 
-	/* (non-Javadoc)
-	 * @see vrpRep.solutionChecker.constraint.IConstraint#evaluate(vrpRep.schema.solution.Solution)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * vrpRep.solutionChecker.constraint.IConstraint#evaluate(vrpRep.schema.
+	 * solution.Solution)
 	 */
-	@Override
-	public void evaluate(InstanceReader inst, DefaultSolution sol) {
-		// TODO Auto-generated method stub
-		
-	}
+	/*
+	 * @Override public void evaluate(InstanceReader inst, DefaultSolution sol)
+	 * { // TODO Auto-generated method stub
+	 * 
+	 * }
+	 */
 
 }
