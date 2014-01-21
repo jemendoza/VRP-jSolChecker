73f2a5e36c7d7e3a567dcc65540c78a2a75e1118
Raphael <Raphael@172.18.114.171>
2014-01-21 16:58:40 +0100

    TravelDistance constraint implementation

--- VrpRepSolutionChecker/src/vrpRep/solutionChecker/constraint/MaxTravelDistance.java
+++ VrpRepSolutionChecker/src/vrpRep/solutionChecker/constraint/MaxTravelDistance.java
@@ -3,7 +3,6 @@
  */
 package vrpRep.solutionChecker.constraint;
 
-import java.math.BigInteger;
 import java.util.List;
 
 import vrpRep.exceptions.MissingAttributeException;
@@ -57,7 +56,6 @@ public class MaxTravelDistance implements IConstraint {
 	 */
 	private boolean evaluateMtd() {
 		boolean result = true;
-		List<Vehicle> fleet = this.inst.getFleet();
 		double travelDist;
 		int nodeId1, nodeId2;
 
@@ -79,16 +77,15 @@ public class MaxTravelDistance implements IConstraint {
 				}
 			}
 			try {
-				if (travelDist > ((IntValue) (fleet.get(0).getAttribute(
-						"maxTravelDistance").get(0))).getValue()) {
+				if (travelDist > ((IntValue) (inst.getFleet().get(0)
+						.getAttribute("maxTravelDistance").get(0))).getValue()) {
 					System.out
 							.println("Max travel distance of vehicle failed on route "
 									+ r.getId());
 					result = false;
 				}
 			} catch (MissingAttributeException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				System.out.println("Missing maxTravelDistance attribute");
 			}
 		}
 		return result;
@@ -101,45 +98,47 @@ public class MaxTravelDistance implements IConstraint {
 	 */
 	private boolean evaluateMtdWithTypes() {
 		boolean result = true;
-		List<Vehicle> fleet = this.inst.getFleet().getVehicle();
-		BigInteger currentType;
+		List<Vehicle> fleet = this.inst.getFleet();
+		int currentType = 0, nodeId1, nodeId2;
 		double travelDist;
 
-		for (int i = 0; i < fleet.size(); i++) { // for each vehicle type
-			currentType = fleet.get(i).getType();
-
-			for (Route r : sol.getRoutes()) { // for each route
-				travelDist = 0;
-
-				if (BigInteger.valueOf(r.getType()) == currentType) { // if
-					// route
-					// requires
-					// current
-					// type
-					for (int j = 0; j < r.getRoute().size() - 1; j++) { // for
-						// each
-						// node
-						// pair
-						try {
-							travelDist += DistanceCalculator
-									.getDistance(inst, BigInteger.valueOf(r
-											.getRoute().get(j)), BigInteger
-											.valueOf(r.getRoute().get(j + 1)));
-						} catch (Exception e) {
-							e.printStackTrace();
+		for (Route r : sol.getRoutes()) {
+			travelDist = 0;
+			for (int j = 0; j < r.getRequests().size() - 1; j++) {
+				try {
+					nodeId1 = ((IntValue) inst.getRequest(
+							r.getRequests().get(j).getId())
+							.getAttribute("node")).getValue();
+					nodeId2 = ((IntValue) inst.getRequest(
+							r.getRequests().get(j + 1).getId()).getAttribute(
+							"node")).getValue();
+					travelDist += DistanceCalculator.getDistance(inst, nodeId1,
+							nodeId2);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			for (int i = 0; i < fleet.size(); i++) {
+				try {
+					currentType = ((IntValue) (fleet.get(i)
+							.getAttribute("Type").get(0))).getValue();
+					if (currentType == r.getType()) {
+						if (travelDist > ((IntValue) (inst.getFleet().get(i)
+								.getAttribute("maxTravelDistance").get(0)))
+								.getValue()) {
+							System.out
+									.println("Max travel distance of vehicle failed on route "
+											+ r.getId());
+							result = false;
 						}
 					}
-				}
-				if (travelDist > Double.valueOf(fleet.get(i)
-						.getMaxTravelDistance().getContent())) {
-					System.out.println("Max travel distance of vehicle "
-							+ fleet.get(i).getType() + " failed on route "
-							+ r.getId());
-					result = false;
+				} catch (MissingAttributeException e) {
+					System.out.println("No type attribute for fleet number "
+							+ i);
+					e.printStackTrace();
 				}
 			}
 		}
-
 		return result;
 	}
 }
