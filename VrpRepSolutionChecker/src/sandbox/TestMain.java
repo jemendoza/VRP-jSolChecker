b3b0c7ceb8cdc9f7da24a598b650812c166a287d
maxhoskins <maxim.hoskins@gmail.com>
2014-02-03 16:03:06 +0100

    ...

--- VrpRepSolutionChecker/src/sandbox/TestMain.java
+++ VrpRepSolutionChecker/src/sandbox/TestMain.java
@@ -32,6 +32,7 @@ public class TestMain {
 		
 		DynamicFactory factory = new DynamicFactory("./config/config.xml");
 		
+		
 		factory.loadConstraints(solCheck);
 		factory.loadObjective(solCheck);
 		factory.setDistanceCalculator();
